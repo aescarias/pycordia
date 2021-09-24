d3407a79ab086e850b888d671eac9a5ccc0e8c44
class PythonAddict <classpythonaddike@gmail.com>
2021-09-24 20:23:09 +0530

    Last commit, hopefully

--- docs/build.sh
+++ docs/build.sh
@@ -14,9 +14,7 @@ export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
  
 # Python Sphinx, configured with source/conf.py
 # See https://www.sphinx-doc.org/
-# sphinx-apidoc -f -o source/ --module-first ../pycordia
-cd docs
-make html
+make generate_docs
 
 #######################
 # Update GitHub Pages #
