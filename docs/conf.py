018d0917422d11c72b81f12d551d4c84d4c40b55
class PythonAddict <classpythonaddike@gmail.com>
2021-09-26 07:20:14 +0530

    Revert pr

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ release = '0.1.2'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
 ]
 
 source_suffix = ['.rst', '.md']
