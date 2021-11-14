0c6be9241c558146a6e39fde71e6e8fb4079c6b2
Ángel Carias <69056344+angelCarias@users.noreply.github.com>
2021-11-13 19:28:35 -0600

    :memo: Version number change

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, Angel Carias'
 author = 'Angel Carias'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.0'
+release = '0.2.1'
 
 
 # -- General configuration ---------------------------------------------------
