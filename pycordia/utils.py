c61ce3ba7ece664e19d5a489ff3160133b8c4804
Ángel Carias <69056344+angelCarias@users.noreply.github.com>
2021-11-25 20:18:52 -0600

    :sparkles: Added `get_flag_list`

--- pycordia/utils.py
+++ pycordia/utils.py
@@ -1,6 +1,29 @@
 import typing
 import inspect
 import datetime
+import enum
+
+
+class BoolEnum:
+    def __new__(cls, **kwargs):       
+        # Get defined class attributes
+
+        attribs = {}
+        
+        for name, value in inspect.getmembers(cls):
+            # If member is not a routine (a function) and if it's not a dunder
+            if not inspect.isroutine(value) and \
+                not (name.startswith("__") and name.endswith("__")):    
+                attribs[name] = value
+        
+        for name, value in kwargs.items():
+            if name not in attribs:
+                raise ValueError(f"'{name}' is Not a valid keyword argument")
+            
+        init = object.__new__(cls)
+        return init
+
+
 
 def obj_from_dict(data: dict, obj: typing.Callable, alias: dict = None):
     """Convert a dictionary `data` into an `obj`.
@@ -21,22 +44,27 @@ def obj_from_dict(data: dict, obj: typing.Callable, alias: dict = None):
 
     return obj(**new_inf)
 
-def obj_to_dict(obj: typing.Any, alias: dict = None):
+def obj_to_dict(obj: typing.Any, *, alias: dict = None, ignore_fields: list = None):
     """Convert an `obj` into a dictionary.
     
     Parameters:
         obj (typing.Any): The object to convert
         alias (dict): A dictionary containing aliases for any parts of the object. \
             Follows a (attribute-alias) pair
+        ignore_fields (list): A list of attributes to ignore when converting
     """
     new_inf = {}
+    if ignore_fields is None:
+        ignore_fields = []
+    
     if obj.__dict__:
         for key, value in obj.__dict__.items():
             if not key.startswith("_"):
-                if alias and key in alias:
-                    new_inf[alias[key]] = value
-                else:
-                    new_inf[key] = value
+                if key not in ignore_fields:
+                    if alias and key in alias:
+                        new_inf[alias[key]] = value
+                    else:
+                        new_inf[key] = value
 
     return new_inf        
 
@@ -87,3 +115,6 @@ def add_ext(hash_: str):
     if hash_.startswith("a_"):
         return f'{hash_}.gif'
     return f'{hash_}.png'
+
+def get_flag_list(enum_flag: enum.EnumMeta, integer: int):
+    return [flag for flag in enum_flag if integer & flag.value == flag.value]  # type: ignore
